1e8428f4334f15c7a9a57dadb001c3798696e6d7
OgelGames <olliverdc28@gmail.com>
2023-01-08 18:21:35 +1100

    make luacheck happy again
    fixes the warning: `interest.lua:10:7: 'not (x == y)' can be replaced by 'x ~= y' (if neither side is a table or NaN)`

--- interest.lua
+++ interest.lua
@@ -7,7 +7,7 @@ minetest.register_globalstep(function(dtime)
 		atm.readaccounts()
 		for _,player in ipairs(minetest.get_connected_players()) do
 			local name = player:get_player_name()
-			if not (atm.balance[name] == nil) then
+			if atm.balance[name] then
 				atm.balance[name] = math.floor(atm.balance[name] + 5)
 			end
 		end
